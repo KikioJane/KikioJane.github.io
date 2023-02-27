20f1013f8114f52959e5cd5244c0ad742ea23b23
Jessica Lynn <lynnjess.dev@gmail.com>
2023-02-26 20:53:22 -0800

    bigger text

--- Assignment4/fancifymytext.js
+++ Assignment4/fancifymytext.js
@@ -1,6 +1,7 @@
 function alertButton()
 {
     alert("Hello, world!");
+    //Add action that makes text bigger when button is pressed
+    document.getElementById("Text box").style.fontSize = "24px";
 }
 
-//Add action that makes text bigger when button is pressed
