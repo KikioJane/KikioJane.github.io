f96a722b7fd5213b38421124f0c0f8f56f446927
Jessica Lynn <lynnjess.dev@gmail.com>
2023-02-26 21:07:56 -0800

    remove alert

--- Assignment4/fancifymytext.js
+++ Assignment4/fancifymytext.js
@@ -1,6 +1,6 @@
 function alertButton()
 {
-    alert("Hello, world!");
+    //alert("Hello, world!");
     //Add action that makes text bigger when button is pressed
     document.getElementById("Text box").style.fontSize = "24px";
 }
