1609fd38922f1b4edff03d0caf1e27893235db2c
Jessica Lynn <lynnjess.dev@gmail.com>
2023-05-19 15:13:22 -0700

    Assignment 6 added

---
+++ Assignment6/starter/encrypt-it.js
@@ -0,0 +1,64 @@
+/*
+ * Starter file 
+ */
+(function() {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+    console.log("Window loaded");
+
+    document.getElementById("encrypt-it").addEventListener("click", handleClick);
+    
+    return;
+  }
+  
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+  
+  function handleClick()
+  {
+    console.log("Button Clicked");
+
+    var input = document.getElementById("input-text").value;
+
+    document.getElementById("result").innerText = shiftCipher(input);
+    
+    return;
+  }
+  
+  function shiftCipher(input)
+  {
+    let output = "";
+    for(let i=0; i < input.length; i++)
+    { 
+      if(input[i] >= 'A' && input[i] <= 'Z')
+      {
+        output += String.fromCharCode(65 + (input.charCodeAt(i) - 65 + 1)%26);
+      }
+      else if (input[i] >= 'a' && input[i] <= 'z')
+      {
+        output += String.fromCharCode(97 + (input.charCodeAt(i) - 97 + 1)%26);
+      }
+      else 
+      {
+        output += input[i];
+      }
+    }
+    
+    return output;
+  }
+
+})();
